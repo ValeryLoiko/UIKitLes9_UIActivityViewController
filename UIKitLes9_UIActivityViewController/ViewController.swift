c06db983ffe507b28efeb3a27db2920ebce2b56a
Valery Loiko <valery.loiko.ios@gmail.com>
2022-09-10 17:18:32 +0200

    Initial Commit

---
+++ UIKitLes9_UIActivityViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitLes9_UIActivityViewController
+//
+//  Created by Diana on 10/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
