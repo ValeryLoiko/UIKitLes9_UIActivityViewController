401ca41844d2ecdd1782cce8979b66e21da880f3
Valery Loiko <valery.loiko.ios@gmail.com>
2022-09-12 23:06:50 +0200

    add code for today

--- UIKitLes9_UIActivityViewController/Model.swift
+++ UIKitLes9_UIActivityViewController/Model.swift
@@ -8,39 +8,41 @@
 import Foundation
 
 
-struct Pizza {
+struct ModelPizza {
     let name: String
     let size: Size
     let price: Double
 }
 
 enum Size: String {
-    case small = "маленькая"
-    case big = "большая"
+    case small = "Маленькая"
+    case big = "Большая"
 }
 
 class Pizzes {
     
-    var arrayPizzes: [Pizza] = []
+    static let shared = Pizzes()
     
+    var arrayPizzes: [ModelPizza] = []
+
     init() {
         setup()
     }
     
     
     private func setup() {
-        let vegaBig = Pizza(name: "Вегетариянская", size: .big, price: 49.90)
-        let vegaSmall = Pizza(name: "Вегетариянская", size: .small, price: 37.90)
-        let miasnajaBig = Pizza(name: "Мясная", size: .big, price: 55.90)
-        let miasnajaSmall = Pizza(name: "Мясная", size: .small, price: 42.90)
-        let peperoniHotBig = Pizza(name: "Пеперони Хот", size: .big, price: 51.90)
-        let peperoniHotSmall = Pizza(name: "Пеперони Хот", size: .small, price: 38.90)
-        let saliamiBig = Pizza(name: "Салями", size: .big, price: 49.40)
-        let saliamiSmall = Pizza(name: "Салями", size: .small, price: 38.90)
-        let sPomidoramBig = Pizza(name: "С помидорам", size: .big, price: 54.90)
-        let sPomidoramSmall = Pizza(name: "С помидорам", size: .small, price: 40.90)
-        let syrnajaBig = Pizza(name: "Сырная", size: .big, price: 55.90)
-        let syrnajaSmall = Pizza(name: "Сырная", size: .small, price: 42.90)
+        let vegaBig = ModelPizza(name: "Вегетариянская", size: .big, price: 49.90)
+        let vegaSmall = ModelPizza(name: "Вегетариянская", size: .small, price: 37.90)
+        let miasnajaBig = ModelPizza(name: "Мясная", size: .big, price: 55.90)
+        let miasnajaSmall = ModelPizza(name: "Мясная", size: .small, price: 42.90)
+        let peperoniHotBig = ModelPizza(name: "Пеперони Хот", size: .big, price: 51.90)
+        let peperoniHotSmall = ModelPizza(name: "Пеперони Хот", size: .small, price: 38.90)
+        let saliamiBig = ModelPizza(name: "Салями", size: .big, price: 49.40)
+        let saliamiSmall = ModelPizza(name: "Салями", size: .small, price: 38.90)
+        let sPomidoramBig = ModelPizza(name: "С помидорам", size: .big, price: 54.90)
+        let sPomidoramSmall = ModelPizza(name: "С помидорам", size: .small, price: 40.90)
+        let syrnajaBig = ModelPizza(name: "Сырная", size: .big, price: 55.90)
+        let syrnajaSmall = ModelPizza(name: "Сырная", size: .small, price: 42.90)
         
         arrayPizzes.append(vegaBig)
         arrayPizzes.append(vegaSmall)
@@ -54,5 +56,7 @@ class Pizzes {
         arrayPizzes.append(sPomidoramSmall)
         arrayPizzes.append(syrnajaBig)
         arrayPizzes.append(syrnajaSmall)
+
     }
+   
 }
--- UIKitLes9_UIActivityViewController/ViewController.swift
+++ UIKitLes9_UIActivityViewController/ViewController.swift
@@ -8,41 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
- 
-    let pizzes = Pizzes()
     
     var imagePizza = UIImageView()
     var segmentControl = UISegmentedControl()
     
+    var currentBigPizza = Pizzes.shared.arrayPizzes[1]
+    var currentSmallPizza = Pizzes.shared.arrayPizzes[0]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .red
-        makeImageView("вегетариянская")
+       
+        makeImageView(currentBigPizza.name)
         makeSegmentControl()
+        
+        updateCurrentPizzes()
     }
+    
+    private func updateCurrentPizzes() {
+        var indexBig = 0
+        var indexSmall = 0
+        
+        print(currentBigPizza.name)
+        print(currentSmallPizza.name)
+        
+        for item in 0..<Pizzes.shared.arrayPizzes.count - 1 {
+            if currentBigPizza.name == Pizzes.shared.arrayPizzes[item].name && currentBigPizza.size == Pizzes.shared.arrayPizzes[item].size{
+                    print(Pizzes.shared.arrayPizzes[item].name)
+                    indexBig = item
+                }
+            }
+        
+        for item in 0..<Pizzes.shared.arrayPizzes.count - 1 {
+            if currentSmallPizza.name == Pizzes.shared.arrayPizzes[item].name && currentSmallPizza.size == Pizzes.shared.arrayPizzes[item].size {
+                print(Pizzes.shared.arrayPizzes[item].name)
+                indexSmall = item
+            }
+        }
+           
+        
+        print("indexBig \(indexBig)")
+       print("indexSmall \(indexSmall)")
+        
+        currentBigPizza = Pizzes.shared.arrayPizzes[indexBig]
+        currentSmallPizza = Pizzes.shared.arrayPizzes[indexSmall]
+        
+        print(currentBigPizza.name)
+        print(currentSmallPizza.name)
+        
+    }
+  
+    
     
     private func makeSegmentControl() {
-        var items = [String: String]()
-        pizzes.arrayPizzes.forEach { item in
-            print(item.name)
-            let price = String(item.price)
-            let size = String(item.size.rawValue)
-            
-            items[size] = price
-        }
-        print(items)
+        var nameForSection = [String]()
+        let bigPizza = "\(currentBigPizza.size.rawValue) - \(currentBigPizza.price) зл"
+        let smallPizza = "\(currentSmallPizza.size.rawValue) - \(currentSmallPizza.price) зл"
+        nameForSection.append(bigPizza)
+        nameForSection.append(smallPizza)
+        
+        segmentControl = UISegmentedControl(items: nameForSection)
+        segmentControl.frame.size = CGSize(width: 300, height: 40)
+        segmentControl.frame.origin = CGPoint(x: view.frame.midX - segmentControl.frame.width / 2, y: imagePizza.frame.maxY + 30)
+        view.addSubview(segmentControl)
+        
     }
     
     
     
-    private func makeImageView(_ namedForImage: String) {
+    private func makeImageView(_ namedForImage: String)  {
         let image = UIImage(named: namedForImage)
         imagePizza.image = image
         imagePizza.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         view.addSubview(imagePizza)
-        
     }
-
 }
 
